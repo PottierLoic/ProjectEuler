830a0943a8a9b19a27f7364c6a5e7786c8996659
PottierLoic <loic.pottier@live.fr>
2024-10-21 23:08:53 +0200

    feat: add 5 6 7

---
+++ 5.v
@@ -0,0 +1,16 @@
+fn main() {
+  mut max := 21
+  for true {
+    mut found := true
+    for i in 2..21 {
+      if (max) % i != 0 {
+        max += 1
+        found = false
+      }
+    }
+    if found {
+      break
+    }
+  }
+  println(max)
+}
